09a250a11a2202f8bc2d73fe1dc089d2ef822584
Bhavesh Yadav <63863797+Thunderbolt9@users.noreply.github.com>
2022-03-02 18:31:59 +0530

    Update users.js

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -7,7 +7,7 @@ const checkAuth = require("../../middleware/check-auth");
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, "./uploads/");
+    callback(null, "./uploads");
   },
   filename: function (req, file, callback) {
     callback(null, new Date().toISOString() + file.originalname);
